f08abc9254f2486791141e70bcb09c32d2c45aee
Andrea Axelsson <120781985+Andrea-Axelsson@users.noreply.github.com>
2024-01-16 13:52:42 +0100

    Updated code

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -14,7 +14,7 @@ function Project(props) {
 ))
 
 const backgroundImage = {
-  backgroundImage: `url(/src/assets/${props.projectImage})`,
+  backgroundImage: `url(/assets/${props.projectImage})`,
   backgroundSize: 'cover',
   backgroundPosition: 'bottom',
   width: '100%',
